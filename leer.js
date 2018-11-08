ec2dc72040aa0d7260b90b2f08c45b0d08a65a75
alexeis <alexeis.carrillo@hotmail.com>
2018-11-07 21:44:40 -0400

    Tareas

--- leer.js
+++ leer.js
@@ -4,10 +4,9 @@ http.createServer(function(req,res){
     fs.readFile("./index.html",(err,html) =>{
         var html_string = html.toString();
         var nombre = "Pdro"
-        var variables  = html_string.match(/[^\{\}]+(?=\})/g);
+        var variables  = html_string.match(/[^\{\}]+(?=\})/g); //{fasdf}
         //var value = eval(variables[0]); 
         html_string = html_string.replace("{" + variables[0] + "}", nombre);
-
 
         res.writeHead(200, { 'Content-Type': 'text/html'});
         res.write(html_string)
